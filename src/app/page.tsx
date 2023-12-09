ef610c8537fef2036adc46cf0460107cf379e1b8
Li-Ninja <lininja.tw@gmail.com>
2023-12-10 00:42:35 +0800

    feat: home image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
+import Image from 'next/image';
+
 export default function Home() {
   return (
-    <div>Home page</div>
+    <div>
+      <div className="flex justify-center items-center self-center h-[50vh]">
+        <Image
+          src="/ttt_51_logo.png"
+          alt="TTT Logo"
+          width="70"
+          height="10"
+          className="w-64 col-12"
+          priority
+          />
+      </div>
+      <div className="text-center text-3xl">桌球神秘 51 區首頁</div>
+    </div>
   );
 }
