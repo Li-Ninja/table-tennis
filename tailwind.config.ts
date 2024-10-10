41638dad3787d282b0c9e732a263822d260f196a
Li-Ninja <lininja.tw@gmail.com>
2024-10-10 17:23:29 +0800

    fix: custom number not work, like pl-10

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,7 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/tw-elements-react/dist/js/**/*.js',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx}', './public/index.html'],
   theme: {
     extend: {
       backgroundImage: {
